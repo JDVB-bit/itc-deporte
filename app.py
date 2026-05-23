5e9e50025cc1d52c204cd458e04f3df256c99da4
JDVB-bit <juandavidvieda2020@gmail.com>
2026-05-22 20:27:41 -0500

    Update page icon in Streamlit configuration

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from data import (
     enf_limpio,
 )
 
-st.set_page_config(page_title="ITC Deportes", page_icon="⚽", layout="wide")
+st.set_page_config(page_title="ITC Deportes", page_icon="static/logo_final.png", layout="wide")
 
 for k, v in [("rol","invitado"),("usuario",None),("tema","oscuro")]:
     if k not in st.session_state:
